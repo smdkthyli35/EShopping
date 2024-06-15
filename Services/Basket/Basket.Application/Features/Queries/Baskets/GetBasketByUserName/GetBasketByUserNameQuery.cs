9f5edf95672eb83d42603f3c6f6459b0ded6d02c
Samed Kütahyalı <smdkthyli@gmail.com>
2024-06-15 14:33:28 +0300

    feat: Add GetBasketByUserName query and mapping profiles

---
+++ Services/Basket/Basket.Application/Features/Queries/Baskets/GetBasketByUserName/GetBasketByUserNameQuery.cs
@@ -0,0 +1,15 @@
+﻿using Basket.Application.Responses;
+using MediatR;
+
+namespace Basket.Application.Features.Queries.Baskets.GetBasketByUserName
+{
+    public class GetBasketByUserNameQuery : IRequest<ShoppingCartResponse>
+    {
+        public string UserName { get; set; }
+
+        public GetBasketByUserNameQuery(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
---
+++ Services/Basket/Basket.Application/Features/Queries/Baskets/GetBasketByUserName/GetBasketByUserNameQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Basket.Application.Mappers;
+using Basket.Application.Responses;
+using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Features.Queries.Baskets.GetBasketByUserName
+{
+    public class GetBasketByUserNameQueryHandler : IRequestHandler<GetBasketByUserNameQuery, ShoppingCartResponse>
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public GetBasketByUserNameQueryHandler(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        public async Task<ShoppingCartResponse> Handle(GetBasketByUserNameQuery request, CancellationToken cancellationToken)
+        {
+            var shoppingCart = await _basketRepository.GetBasket(request.UserName);
+            var shoppingCartResponse = BasketMapper.Mapper.Map<ShoppingCartResponse>(shoppingCart);
+            return shoppingCartResponse;
+        }
+    }
+}
