75dff28460e350a439b45ad09876936e9e9bc967
Samed Kütahyalı <smdkthyli@gmail.com>
2024-06-15 14:40:45 +0300

    feat: Add CreateShoppingCartCommand and handler

---
+++ Services/Basket/Basket.Application/Features/Commands/Baskets/CreateShoppingCart/CreateShoppingCartCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Basket.Application.Mappers;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
+using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Features.Commands.Baskets.CreateShoppingCart
+{
+    public class CreateShoppingCartCommandHandler : IRequestHandler<CreateShoppingCartCommand, ShoppingCartResponse>
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public CreateShoppingCartCommandHandler(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        public async Task<ShoppingCartResponse> Handle(CreateShoppingCartCommand request, CancellationToken cancellationToken)
+        {
+            ShoppingCart shoppingCart = await _basketRepository.UpdateBasket(new()
+            {
+                UserName = request.UserName,
+                Items = request.Items,
+            });
+
+            ShoppingCartResponse shoppingCartResponse = BasketMapper.Mapper.Map<ShoppingCartResponse>(shoppingCart);
+            return shoppingCartResponse;
+        }
+    }
+}
